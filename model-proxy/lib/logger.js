631b0400035aead05d3b2cdfcc760ec3d3fcda63
Justin Merz <jrmerz@gmail.com>
2026-03-18 14:12:12 -0700

    simplified setup with keycloak

--- model-proxy/lib/logger.js
+++
@@ -1,9 +0,0 @@
-import {createLogger} from '@ucd-lib/logger';
-import config from './config.js';
-
-const logger = createLogger({
-  name : config.logger.name,
-  level : config.logger.level
-});
-
-export default logger;
